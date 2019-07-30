7915dcb1c0e009dd6506f4bb18dff489c21fe10a
Administrador <Administrador@10.10.4.10>
2019-07-30 16:44:06 -0300

    1

---
+++ ator/src/controlador/ControladorAtor.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import tela.manutencao.ManutencaoAtor;
+import dao.AtorDao;
+import javax.swing.JOptionPane;
+import modelo.Ator;
+import tela.manutencao.ManutencaoAtor;
+/**
+ *
+ * @author Administrador
+ */
+public class ControladorAtor {
+
+    public static void inserir(ManutencaoAtor aThis) {
+        public static void inserir(ManutencaoAtor man){
+        Ator objeto = new Ator();
+        objeto.setNome_artistico(man.jtfNomeArtistico.getText());
+        objeto.setNome_real(man.jtfNomeReal.getText());
+        
+        boolean resultado = AtorDao.inserir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+  
+    
+}
---
+++ ator/src/modelo/Ator.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Ator {
+    
+    private int codigo;
+    private String nome_artistico;
+    private String nome_real;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome_artistico() {
+        return nome_artistico;
+    }
+
+    public void setNome_artistico(String nome_artistico) {
+        this.nome_artistico = nome_artistico;
+    }
+
+    public String getNome_real() {
+        return nome_real;
+    }
+
+    public void setNome_real(String nome_real) {
+        this.nome_real = nome_real;
+    }
+
+    @Override
+    public String toString() {
+        return "Ator{" + "codigo=" + codigo + ", nome_artistico=" + nome_artistico + ", nome_real=" + nome_real + '}';
+    }
+    
+    
+}
